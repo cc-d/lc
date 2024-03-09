59f487757e0223c1adb4a88b962f70e8ed5ce307
Cary Carter <ccarterdev@gmail.com>
2024-03-09 13:28:24 -0800

    first

---
+++ 1492.py
@@ -0,0 +1,15 @@
+"""1492. The kth Factor of n"""
+
+
+class Solution:
+    def kthFactor(self, n: int, k: int) -> int:
+        curk = 0
+        for i in range(n, 0, -1):
+            div = n / i
+            if div.is_integer():
+                curk += 1
+                print(div, n, i, curk)
+                if curk == k:
+                    return int(div)
+
+        return -1
